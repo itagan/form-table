203f3141389eed6430646555ca8aa8ac9842fb7c
itagan <841114100@qq.com>
2026-08-21 23:12:19 +0800

    docs: add custom component binding demo

---
+++ playground/src/components/CustomComponents/DateRangePicker.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-date-picker
+    :value="range"
+    type="daterange"
+    value-format="yyyy-MM-dd"
+    range-separator="至"
+    start-placeholder="开始日期"
+    end-placeholder="结束日期"
+    clearable
+    style="width: 100%;"
+    @input="handleInput"
+  />
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  range?: Array<string | null> | null
+}>()
+
+const emit = defineEmits<{
+  (event: 'range-change', range: string[] | null): void
+}>()
+
+const handleInput = (range: string[] | null) => {
+  emit('range-change', range)
+}
+</script>
